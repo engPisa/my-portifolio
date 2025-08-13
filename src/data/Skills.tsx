1139df6bd425e9406c44349cdb1344e892caf558
devPisa <cesar.pisa@alunos.bpkedu.com.br>
2025-08-13 02:47:19 -0300

    refactor: skill components

--- src/data/Skills.tsx
+++ src/data/Skills.tsx
@@ -5,19 +5,17 @@ export interface Skill {
 }
 
 export const skills: Skill[] = [
+  { name: 'Java', icon: 'java', url: 'https://docs.oracle.com/javase/' },
+  { name: 'Python', icon: 'python', url: 'https://docs.python.org/3/' },
   { name: 'JavaScript', icon: 'js', url: 'https://developer.mozilla.org/docs/Web/JavaScript' },
   { name: 'Node.js', icon: 'nodejs', url: 'https://nodejs.org/docs/latest/api/' },
-  { name: 'TypeScript', icon: 'ts', url: 'https://www.typescriptlang.org/docs/' },
-  { name: 'Next.js', icon: 'nextjs', url: 'https://nextjs.org/docs' },
-  { name: 'Python', icon: 'python', url: 'https://docs.python.org/3/' },
-  { name: 'Java', icon: 'java', url: 'https://docs.oracle.com/javase/' },
-  { name: 'Docker', icon: 'docker', url: 'https://docs.docker.com/' },
-  { name: 'Azure', icon: 'azure', url: 'https://learn.microsoft.com/azure/' },
-  { name: 'React', icon: 'react', url: 'https://react.dev/learn' },
-  { name: 'PostgreSQL', icon: 'postgres', url: 'https://www.postgresql.org/docs/' },
-  { name: 'MySQL', icon: 'mysql', url: 'https://dev.mysql.com/doc/' },
-  { name: 'MongoDB', icon: 'mongodb', url: 'https://www.mongodb.com/docs/' },
-  { name: 'Redis', icon: 'redis', url: 'https://redis.io/docs/' },
   { name: 'Express', icon: 'express', url: 'https://expressjs.com/' },
+  { name: 'TypeScript', icon: 'ts', url: 'https://www.typescriptlang.org/docs/' },
+  { name: 'React', icon: 'react', url: 'https://react.dev/learn' },
+  { name: 'Next.js', icon: 'nextjs', url: 'https://nextjs.org/docs' },
   { name: 'Tailwind CSS', icon: 'tailwind', url: 'https://tailwindcss.com/docs' },
+  { name: 'Docker', icon: 'docker', url: 'https://docs.docker.com/' },
+  { name: 'MySQL', icon: 'mysql', url: 'https://dev.mysql.com/doc/' },
+  { name: 'PostgreSQL', icon: 'postgres', url: 'https://www.postgresql.org/docs/' },
+  { name: 'MongoDB', icon: 'mongodb', url: 'https://www.mongodb.com/docs/' },
 ];
