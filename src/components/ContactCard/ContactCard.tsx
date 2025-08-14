a15cf7114a1aa1a4c875425f8b790e96ee24412b
devPisa <cesar.pisa@alunos.bpkedu.com.br>
2025-08-14 11:41:41 -0300

    Fix: component card and button

--- src/components/ContactCard/ContactCard.tsx
+++ src/components/ContactCard/ContactCard.tsx
@@ -1,34 +1,34 @@
 import Button from '@/components/Button';
+import { Send, Github , Linkedin } from 'lucide-react';
 
 export default function ContactCard() {
   return (
-    <section className="bg-surface border border-border rounded-xl p-8 w-full text-center max-w-2xl mx-auto">
+    <section className="bg-[#fdfaf6] border border-[#e6dcd5] rounded-xl p-6 w-full cursor-pointer transition-all duration-300 hover:shadow-md">
       
       <h2 className="text-4xl font-serif font-normal text-text-primary">
         Contato
       </h2>
 
       <p className="text-text-secondary mt-2 mb-6">
-        Disponível para projetos globais, remoto ou em Sydney, Austrália.
+        Disponível para projetos. Contate-me!
       </p>
 
-      <div className="flex justify-center items-center gap-4">
-        <a href="mailto:seu-email-aqui@example.com">
-          <Button variant="primary">
+      <div className="flex  items-center gap-4">
+        <a href="mailto:seu-email-cpisa.eng@hotmail.com">
+          <Button variant="primary" iconLeft={<Send size={20}/>}>
             Enviar e-mail
           </Button>
         </a>
-        <a href="https://github.com/seu-usuario" target="_blank" rel="noopener noreferrer">
-          <Button variant="secondary">
+        <a href="https://github.com/engpisa" target="_blank" rel="noopener noreferrer">
+          <Button variant="secondary" iconLeft={<Github  size={20}/>}>
             GitHub
           </Button>
         </a>
-        <a href="https://linkedin.com/in/seu-usuario" target="_blank" rel="noopener noreferrer">
-          <Button variant="secondary">
+        <a href="https://linkedin.com/in/engpisa" target="_blank" rel="noopener noreferrer">
+          <Button variant="secondary" iconLeft={<Linkedin size={20}/>}>
             LinkedIn
           </Button>
         </a>
-
       </div>
     </section>
   );
